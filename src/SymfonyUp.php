58ebe6f5c098c7dac1ad8c4f464a4b058f73b491
Petr Knap <dev@petrknap.cz>
2017-06-02 13:18:20 +0200

    Implemented SymfonyUp

---
+++ src/SymfonyUp.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Netpromotion\SymfonyUp;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class SymfonyUp
+{
+    /**
+     * @var callable
+     */
+    private $kernelFactory;
+
+    private function __construct(callable $kernelFactory)
+    {
+        $this->kernelFactory = $kernelFactory;
+    }
+
+    /**
+     * @param callable $kernelFactory
+     * @return static
+     */
+    public static function viaKernelFactory($kernelFactory)
+    {
+        return new static($kernelFactory);
+    }
+
+    /**
+     * @param string $kernelClass
+     * @return static
+     */
+    public static function viaKernelClass($kernelClass)
+    {
+        return new static(function ($environment, $debug) use ($kernelClass) {
+            return new $kernelClass($environment, $debug);
+        });
+    }
+
+    /**
+     * @param KernelInterface $kernel
+     * @return static
+     */
+    public static function viaKernel(KernelInterface $kernel)
+    {
+        return new static(function () use ($kernel) {
+            return $kernel;
+        });
+    }
+
+    /**
+     * Prepares and runs web application
+     *
+     * @param string $environment
+     * @param bool $debug
+     */
+    public function runWeb($environment = 'prod', $debug = false)
+    {
+        if ($debug) {
+            Debug::enable();
+        }
+
+        /** @var KernelInterface $kernel */
+        $kernel = call_user_func($this->kernelFactory, $environment, $debug);
+        $this->checkKernel($kernel, $environment, $debug);
+
+        if (!$debug) {
+            if (method_exists($kernel, 'loadClassCache')) {
+                $kernel->loadClassCache();
+            }
+
+            $kernel = new HttpCache($kernel);
+        }
+
+        $request = Request::createFromGlobals();
+        $response = $kernel->handle($request);
+        $response->send();
+        $kernel->terminate($request, $response);
+    }
+
+    /**
+     * Prepares and runs console application
+     *
+     * @param InputInterface|null $input
+     * @param OutputInterface|null $output
+     * @param bool $autoExit
+     * @return int
+     */
+    public function runConsole(InputInterface $input = null, OutputInterface $output = null, $autoExit = true)
+    {
+        set_time_limit(0);
+
+        if (null === $input) {
+            $input = new ArgvInput();
+        }
+
+        $environment = $input->getParameterOption(['--env', '-e'], getenv('SYMFONY_ENV') ?: 'dev');
+        $debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(['--no-debug', '']) && $environment !== 'prod';
+
+        if ($debug) {
+            Debug::enable();
+        }
+
+        /** @var KernelInterface $kernel */
+        $kernel = call_user_func($this->kernelFactory, $environment, $debug);
+        $this->checkKernel($kernel, $environment, $debug);
+
+        $application = new Application($kernel);
+        $application->setAutoExit($autoExit);
+
+        return $application->run($input, $output);
+    }
+
+    /**
+     * @param KernelInterface $kernel
+     * @param string $environment
+     * @param bool $debug
+     */
+    private function checkKernel(KernelInterface $kernel, $environment, $debug)
+    {
+        if ($kernel->getEnvironment() !== $environment) {
+            user_error(sprintf(
+                'The environment is "%s", expected "%s"',
+                $kernel->getEnvironment(),
+                $environment
+            ), E_USER_ERROR);
+        }
+
+        if ($kernel->isDebug() !== $debug) {
+            user_error(sprintf(
+                'The debug is %s, expected %s',
+                var_export($kernel->isDebug(), true),
+                var_export($debug, true)
+            ), E_USER_WARNING);
+        }
+    }
+}
---
+++ tests/SymfonyUpTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Netpromotion\SymfonyUp\Test;
+
+use Netpromotion\SymfonyUp\SymfonyUp;
+use Netpromotion\SymfonyUp\Test\SomeApp\SomeKernel;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * @runTestsInSeparateProcesses otherwise the error handler will be changed
+ */
+class SymfonyUpTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider dataViaWorks
+     * @param string $method
+     * @param mixed $input
+     */
+    public function testViaWorks($method, $input)
+    {
+        SymfonyUp::{$method}($input);
+    }
+
+    public function dataViaWorks()
+    {
+        return [
+            ["viaKernelFactory", [$this, __FUNCTION__]],
+            ["viaKernelClass", __CLASS__],
+            ["viaKernel", new SomeKernel('dev', true)]
+        ];
+    }
+
+    /**
+     * @dataProvider dataKernelFactoryGetsEnvironmentAndDebug
+     * @param string $environment
+     * @param bool $debug
+     */
+    public function testKernelFactoryGetsEnvironmentAndDebug($environment, $debug)
+    {
+        $factoryCalled = false;
+        try {
+            SymfonyUp::viaKernelFactory(function ($a, $b) use ($environment, $debug, &$factoryCalled) {
+                $this->assertEquals($environment, $a);
+                $this->assertEquals($debug, $b);
+
+                $factoryCalled = true;
+
+                return new SomeKernel($environment, $debug);
+            })->runWeb($environment, $debug);
+        } catch (NotFoundHttpException $ignored) {
+            // There is no route for /
+        }
+        $this->assertTrue($factoryCalled);
+    }
+
+    public function dataKernelFactoryGetsEnvironmentAndDebug()
+    {
+        return [
+            ['dev', true],
+            ['dev', false],
+            ['prod', false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataRunWebWorks
+     * @param string $environment
+     * @param bool $debug
+     */
+    public function testRunWebWorks($environment, $debug)
+    {
+        $this->setExpectedException(NotFoundHttpException::class); // There is no route for /
+
+        SymfonyUp::viaKernelClass(SomeKernel::class)->runWeb($environment, $debug);
+    }
+
+    public function dataRunWebWorks()
+    {
+        return [
+            ['dev', true],
+            ['dev', false],
+            ['prod', false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataRunConsoleWorks
+     * @param string $environment
+     * @param bool $debug
+     */
+    public function testRunConsoleWorks($environment, $debug)
+    {
+        $input = new ArrayInput([
+            'command' => 'about',
+            '--env' => $environment,
+            '--' . ($debug ? '' : 'no-') . 'debug',
+        ]);
+
+        $output = new BufferedOutput();
+
+        SymfonyUp::viaKernelClass(SomeKernel::class)->runConsole($input, $output, false);
+
+        $this->assertStringMatchesFormat(
+            '%aEnvironment%w' . $environment . '%aDebug%w' . var_export($debug, true) . '%a',
+            $output->fetch()
+        );
+    }
+
+    public function dataRunConsoleWorks()
+    {
+        return [
+            ['dev', true],
+            ['dev', false],
+            ['prod', false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataCheckKernelWorks
+     * @param KernelInterface $kernel
+     * @param string $environment
+     * @param bool $debug
+     * @param string $expectedException
+     */
+    public function testCheckKernelWorks($kernel, $environment, $debug, $expectedException)
+    {
+        try {
+            SymfonyUp::viaKernel($kernel)->runWeb($environment, $debug);
+
+            $this->fail('Exception was expected');
+        } catch (\Exception $e) {
+            $this->assertContains($expectedException, $e->getMessage());
+        }
+    }
+
+    public function dataCheckKernelWorks()
+    {
+        return [
+            [new SomeKernel('dev', true), 'dev', true, 'No route found for "GET /"'],
+            [new SomeKernel('dev', true), 'dev', false, 'The debug is true, expected false'],
+            [new SomeKernel('dev', true), 'prod', true, 'The environment is "dev", expected "prod"'],
+            [new SomeKernel('dev', true), 'prod', false, 'The environment is "dev", expected "prod"'],
+        ];
+    }
+}
