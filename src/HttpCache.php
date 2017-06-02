d2d38124478c7f3b104b20dc533ee88f17418f25
Petr Knap <dev@petrknap.cz>
2017-06-02 13:08:05 +0200

    Added HttpCache

---
+++ src/HttpCache.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Netpromotion\SymfonyUp;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache as AbstractHttpCache;
+
+class HttpCache extends AbstractHttpCache
+{
+    // Empty class
+}
