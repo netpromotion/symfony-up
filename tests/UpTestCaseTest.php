0af7d6ce3976df62023dc5f552ff40845a6bba6c
Petr Knap <dev@petrknap.cz>
2018-07-02 14:28:08 +0200

    Reconfigured for Symfony 4

--- tests/UpTestCaseTest.php
+++ tests/UpTestCaseTest.php
@@ -7,6 +7,7 @@ use Netpromotion\SymfonyUp\Test\SomeApp\SomeKernel;
 use Netpromotion\SymfonyUp\Test\SomeBundle\Service\SomeService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class UpTestCaseTest extends UpTestCase
 {
@@ -15,33 +16,46 @@ class UpTestCaseTest extends UpTestCase
         return SomeKernel::class;
     }
 
-    public function testKernelContainsContainer()
+    public function testKernelIsKernel()
+    {
+        $this->assertInstanceOf(
+            KernelInterface::class,
+            $this->getKernel()
+        );
+    }
+
+    public function testContainerIsContainer()
     {
         $this->assertInstanceOf(
             ContainerInterface::class,
-            $this->getKernel()->getContainer()
+            $this->getContainer()
         );
     }
 
     /**
-     * @dataProvider dataKernelContainsServices
+     * @dataProvider dataContainerContainsServices
      * @param string $id
      * @param string $expected
      */
-    public function testKernelContainsServices($id, $expected)
+    public function testContainerContainsServices($id, $expected)
     {
         $this->assertInstanceOf(
             $expected,
-            $this->getKernel()->getContainer()->get($id)
+            $this->getContainer()->get($id)
         );
     }
 
-    public function dataKernelContainsServices()
+    public function dataContainerContainsServices()
     {
         return [
             'service from bundle' => ['some_service', SomeService::class],
             'service from test env' => ['test_service', \stdClass::class],
         ];
+    }
+
+    public function testContainerContainsEnvVariablesFromPhpunitXml()
+    {
+        $this->assertSame('From phpunit.xml file', $this->getContainer()->getParameter('TEST_VAR_XML'));
     }
 
     public function testKernelContainsRoutes()
