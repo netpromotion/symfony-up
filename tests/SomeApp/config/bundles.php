0396561d6ef2d83af16c893158c4dece5c534afd
Petr Knap <dev@petrknap.cz>
2018-07-02 11:53:48 +0200

    Extra Bundle is now not necessary for @Route annotations

--- tests/SomeApp/config/bundles.php
+++ tests/SomeApp/config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     \Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     \Netpromotion\SymfonyUp\Test\SomeBundle\SomeBundle::class => ['all' => true],
 ];
