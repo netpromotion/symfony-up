556a172b945d6caee2ff424c6bafe5e7ec9b0b3b
Petr Knap <dev@petrknap.cz>
2018-07-04 14:10:21 +0200

    Fixed "Make sure annotations are installed and enabled."

--- tests/SomeApp/config/bundles.php
+++ tests/SomeApp/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     \Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     \Netpromotion\SymfonyUp\Test\SomeBundle\SomeBundle::class => ['all' => true],
+    \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true]
 ];
